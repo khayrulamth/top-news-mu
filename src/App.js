63bd10dba31ad44d8b996be726f496d607b04f95
Md. Khayrul Islam <devkhayrul@gmail.com>
2021-06-24 01:25:43 +0600

    top news with material ui done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import News from './components/News/News';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Header from './components/Header/Header';
 
 function App() {
+  const [articles,setArticles] = useState([]);
+
+  useEffect(() =>{
+    fetch("https://newsapi.org/v2/everything?q=tesla&from=2021-05-23&sortBy=publishedAt&apiKey=a49cdfad2403431087d4bccb284754b3")
+    .then(res =>res.json())
+    .then(data =>{setArticles(data.articles);})
+  },[])
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header ></Header>
+      {
+        articles.map(article =><News key={article.title} article={article}></News>)
+      }
     </div>
-  );
-}
-
+  )
+  };
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    
+    return (
+        <div>
+            <h2>Today's Top News </h2>
+           
+        </div>
+    );
+};
+
+export default Header;
